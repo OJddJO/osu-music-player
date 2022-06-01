93c8d83cec8cbcfb1f8e8f2826d06b2e1ce5a603
OJddJO <95376647+OJddJO@users.noreply.github.com>
2022-06-01 09:40:47 +0200

    a

--- osu!player/osu!player.py
+++ osu!player/osu!player.py
@@ -1,8 +1,8 @@
 from pygame import mixer
 from tkinter import *
 from pypresence import Presence
-from time import time, sleep
-import asyncio
+from time import time
+from random import randint
 import os
 import export_osu_song
 import threading
@@ -19,8 +19,11 @@ channel = None
 #RPC
 start = time()
 clientid = '980519752025931836'
-RPC = Presence(clientid)
-RPC.connect()
+try:
+    RPC = Presence(clientid)
+    RPC.connect()
+except:
+    pass
 run = True
 
 def changeStatus():
@@ -57,10 +60,9 @@ def importSongs():
     temp_song=export_osu_song.export()
 
     for s in temp_song:
-        if s != 'AlbumArtSmall.jpg' or s != 'Folder.jpg':
-            s=s.replace(f"C:/Users/{user}/Music/osu!player/Osu/","")
-            songs_list.insert(END,s)
-            slist.append(s)
+        s=s.replace(f"C:/Users/{user}/Music/osu!player/Osu/","")
+        songs_list.insert(END,s)
+        slist.append(s)
 
 
 def deletesong():
@@ -83,12 +85,14 @@ def Play():
         song=f'C:/Users/{user}/Music/osu!player/Osu/{song}'
         song = mixer.Sound(song)
         channel.play(song)
+    nowplaying.set(f"{state}: {desc}")
 
 
 def Pause():
     channel.pause()
     global  state
     state = "Paused"
+    nowplaying.set(f"{state}: {desc}")
 
 
 def Stop():
@@ -98,14 +102,22 @@ def Stop():
     global state, desc
     state = "Idle"
     desc = "    "
+    nowplaying.set(f"{state}")
 
 
 def Previous():
     channel.pause()
     channel.stop()
-    previous_one=songs_list.curselection()
-    previous_one=previous_one[0]-1
-    temp2=songs_list.get(previous_one)
+    global prevx, slist, shuffle
+    if shuffle:
+        temp2 = slist[prevx[-1]]
+        previous_one = songs_list.index(prevx[-1])
+        if len(prevx)!=1:
+            prevx.pop()
+    else:
+        previous_one=songs_list.curselection()
+        previous_one=previous_one[0]-1
+        temp2=songs_list.get(previous_one)
     global state, desc
     state = "Listening"
     desc = temp2
@@ -113,16 +125,25 @@ def Previous():
     song = mixer.Sound(temp2)
     channel.play(song)
     songs_list.selection_clear(0,END)
+    songs_list.see(previous_one)
     songs_list.activate(previous_one)
     songs_list.selection_set(previous_one)
+    nowplaying.set(f"{state}: {desc}")
 
 
 def Next():
     channel.pause()
     channel.stop()
-    next_one=songs_list.curselection()
-    next_one=next_one[0]+1
-    temp=songs_list.get(next_one) 
+    global x, shuffle, prevx, slist
+    if shuffle:
+        prevx.append(slist.index(songs_list.get(ACTIVE)))
+        x = randint(0, len(slist)-1)
+        temp = slist[x]
+        next_one = songs_list.index(x)
+    else:
+        next_one=songs_list.curselection()
+        next_one=next_one[0]+1
+        temp=songs_list.get(next_one) 
     global state, desc
     state = "Listening"
     desc = temp
@@ -130,24 +151,41 @@ def Next():
     song = mixer.Sound(temp)
     channel.play(song)
     songs_list.selection_clear(0,END)
+    songs_list.see(next_one)
     songs_list.activate(next_one)
     songs_list.selection_set(next_one)
+    nowplaying.set(f"{state}: {desc}")
 
 
 loop = True
 def Loop():
-    global loop, txt
+    global loop, looptxt
     if loop == True:
         loop = False
-        txt.set("Loop = False")
+        looptxt.set("🔁:❎")
     elif loop == False:
         loop = True
-        txt.set("Loop = True")
+        looptxt.set("🔁:✅")
+
+
+x=0
+prevx = []
+shuffle = True
+def Shuffle():
+    global shuffle, shuffletxt
+    if shuffle == True:
+        shuffle = False
+        shuffletxt.set("🔁:❎")
+    elif shuffle == False:
+        shuffle = True
+        shuffletxt.set("🔀:✅")
 
 
 root=Tk()
 root.title('osu!player')
 root.resizable(False, False)
+root.config(bg="gray15")
+
 
 inactive_ticks = 0
 nosong = False
@@ -155,44 +193,70 @@ nosong = False
 mixer.init()
 channel = mixer.Channel(1)
 
-songs_list=Listbox(root,selectmode=SINGLE,bg="gray15",fg="white",font=('arial',15),height=12,width=66,selectbackground="gray",selectforeground="black")
-songs_list.grid(columnspan=9)
+songs_list=Listbox(root,selectmode=SINGLE,bg="gray15",fg="white",bd=0,highlightthickness=0,font=('arial',15),height=12,width=55,selectbackground="gray",selectforeground="black")
+songs_list.grid(columnspan=7)
+
+nowplaying=StringVar()
+nowplaying.set(f"{state}")
+playing_label=Label(root,textvariable=nowplaying,width=35,bg="gray15",fg="white",bd=2,highlightthickness=0,font=('arial', 15), relief='groove')
+playing_label.grid(row=1, column=0, columnspan=5, pady=5)
+
+play_button=Button(root,text="▶",width =4,command=Play)
+play_button.config(font=('arial',20),bg="gray40",fg="white",bd=0,highlightthickness=1)
+play_button.grid(row=2,column=0, padx=5)
 
 
-play_button=Button(root,text="Play",width =7,command=Play)
-play_button.config(font=('arial',15),bg="gray40",fg="white")
-play_button.grid(row=1,column=0)
+pause_button=Button(root,text="⏸️",width =4,command=Pause)
+pause_button.config(font=('arial',20),bg="gray40",fg="white",bd=0,highlightthickness=1)
+pause_button.grid(row=2,column=1, padx=5)
 
 
-pause_button=Button(root,text="Pause",width =7,command=Pause)
-pause_button.config(font=('arial',15),bg="gray40",fg="white")
-pause_button.grid(row=1,column=1)
+stop_button=Button(root,text="⏹️",width =4,command=Stop)
+stop_button.config(font=('arial',20),bg="gray40",fg="white",bd=0,highlightthickness=1)
+stop_button.grid(row=2,column=2, padx=5)
 
 
-stop_button=Button(root,text="Stop",width =7,command=Stop)
-stop_button.config(font=('arial',15),bg="gray40",fg="white")
-stop_button.grid(row=1,column=2)
+previous_button=Button(root,text="⏮️",width =4,command=Previous)
+previous_button.config(font=('arial',20),bg="gray40",fg="white",bd=0,highlightthickness=1)
+previous_button.grid(row=2,column=3, padx=5)
 
 
-previous_button=Button(root,text="Prev",width =7,command=Previous)
-previous_button.config(font=('arial',15),bg="gray40",fg="white")
-previous_button.grid(row=1,column=3)
+next_button=Button(root,text="⏭️",width =4,command=Next)
+next_button.config(font=('arial',20),bg="gray40",fg="white",bd=0,highlightthickness=1)
+next_button.grid(row=2,column=4, padx=5)
+
+looptxt = StringVar()
+looptxt.set("🔁:✅")
+loop_button=Button(root,textvariable=looptxt,width=5,command=Loop)
+loop_button.config(font=('arial',20),bg="gray40",fg="white",bd=0,highlightthickness=1)
+loop_button.grid(row=2,column=5, padx=5)
+
+shuffletxt = StringVar()
+shuffletxt.set("🔀:✅")
+shuffle_button=Button(root,textvariable=shuffletxt,width=5,command=Shuffle)
+shuffle_button.config(font=('arial',20),bg="gray40",fg="white",bd=0,highlightthickness=1)
+shuffle_button.grid(row=2,column=6)
 
 
-next_button=Button(root,text="Next",width =7,command=Next)
-next_button.config(font=('arial',15),bg="gray40",fg="white")
-next_button.grid(row=1,column=4)
+voltxt = Label(root, bg="gray15", fg="white", text="Volume:")
+voltxt.config(font=('arial',12),bd=0,highlightthickness=0)
+voltxt.grid(row=1, column=5, pady=5)
 
+volume = Scale(root, from_=0, to=100, orient=HORIZONTAL, variable=IntVar)
+volume.grid(row=1, column=6, pady=5)
+volume.config(bg="gray15", fg="white", bd=0, highlightthickness=0)
+volume.set(100)
 
-loop_button=Button(root,text="Loop",width=7,command=Loop)
-loop_button.config(font=('arial',15),bg="gray40",fg="white")
-loop_button.grid(row=1,column=5)
+regvol = 100
 
-txt = StringVar()
-txt.set("Loop = True")
-label = Label(root, textvariable=txt, width=12)
-label.config(font=('arial',15),bg="gray40",fg="white")
-label.grid(row=1, column=6)
+def changeVol():
+    global channel, regvol, volume
+    regvol = int(volume.get())
+    if channel.get_volume() == regvol/100:
+        pass
+    else:
+        channel.set_volume(regvol/100)
+
 
 my_menu=Menu(root)
 root.config(menu=my_menu)
@@ -211,6 +275,8 @@ root.protocol("WM_DELETE_WINDOW", shutdown)
 
 importSongs()
 
+root.iconbitmap(fr"C:\Users\{user}\Music\osu!player\osu-icon-28.ico")
+
 threadA = threading.Thread(target= changeStatus)
 threadA.start()
 
@@ -218,3 +284,4 @@ threadA.start()
 while run:
     root.update()
     testPlaying()
+    changeVol()
