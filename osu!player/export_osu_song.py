f9b79f18c4750319824627c03d54307eac3845b9
OJddJO <95376647+OJddJO@users.noreply.github.com>
2022-06-01 22:25:14 +0200

    better import

--- osu!player/export_osu_song.py
+++ osu!player/export_osu_song.py
@@ -13,49 +13,61 @@ def export():
 
     files = os.listdir(src)
 
+    try:
+        test = eval(open(f'C:\\Users\\{user}\\Music\\osu!player\\import.data').read())
+    except:
+        test = []
 
+    files2 = []
     for dirname in files:
-        i = dirname.index('-')
-        title = dirname[i+2:]
-        titles.append(title)
+        if dirname not in test:
+            test.append(dirname)
+            i = dirname.index(r' - ')
+            title = dirname[i+3:]
+            titles.append(title)
+            files2.append(dirname)
 
-    count = 0
 
-    for songdir in files:
-        path = src + '\\' + songdir
-        for f in os.listdir(path):
-            if f.endswith("3") or f.find("audio") == 0:
-                tmpsrc = path+'\\'+f
-                tmpdst = dst+'\\'+titles[count]+'.mp3'
+    open(f'C:\\Users\\{user}\\Music\\osu!player\\import.data', 'w').write(str(test))
 
-                if f.endswith("3"):
-                    shutil.copy(tmpsrc, tmpdst)
-                else:
-                    song = AudioSegment.from_file(tmpsrc)
-                    song.export(tmpdst, format="mp3")
-                
-                song = eyed3.load(tmpdst)
-                if not song.tag:
-                    song.initTag()
-                song.tag.album = 'OSU!'
-                song.tag.track_num = count + 1
-                song.tag.save()
+    if titles == []:
+        count = -1
+    else:
+        count = 0
 
-                print(str(count)+"/"+str(len(titles)-1) + ':' + titles[count] + '        ' + songdir)
-                count += 1
+    if count!=-1:
+        for songdir in files2:
+            path = src + '\\' + songdir
+            for f in os.listdir(path):
+                if f.endswith("3") or f.find("audio") == 0:
+                    tmpsrc = path+'\\'+f
+                    tmpdst = dst+'\\'+titles[count]+'.mp3'
+
+                    if f.endswith("3"):
+                        shutil.copy(tmpsrc, tmpdst)
+                    else:
+                        song = AudioSegment.from_file(tmpsrc)
+                        song.export(tmpdst, format="mp3")
+                    
+                    song = eyed3.load(tmpdst)
+                    if not song.tag:
+                        song.initTag()
+                    song.tag.album = 'OSU!'
+                    song.tag.save()
+
+                    print(str(count)+"/"+str(len(titles)-1) + ':' + titles[count] + '        ' + songdir)
+                    count += 1
 
     files = os.listdir(dst)
     test = []
 
     for song in files:
-        if song not in test:
-            if song.endswith("mp3"):
-                test.append(song)
-            else:
-                path = dst + "\\" + song
-                os.remove(path)
+        if song.endswith("mp3"):
+            test.append(song)
         else:
             path = dst + "\\" + song
             os.remove(path)
-    files = test
+    else:
+        path = dst + "\\" + song
+        os.remove(path)
     return files
